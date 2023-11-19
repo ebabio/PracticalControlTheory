a1d36f19d593ba421fd9b3686bb02f26275c8ca4
Enrique Babio <enrique.babio@skydweller.aero>
2023-11-19 12:03:04 +0100

    added an initial controller to the pendulum.slx

--- src/code/callbacks/init_pendulum.m
+++ src/code/callbacks/init_pendulum.m
@@ -8,6 +8,17 @@ clear src_path;
 
 %% Load data
 
+% plant
+plant.I = 1; % pendulum MOI
+plant.y = 0; % pendulum CG position
+plant.g = 1; % gravity
+
+% controller
+control.y_scheduling = [-1. 1];
+control.k = zeros(1, 4, 2);
+
+
+%% Load model
+
 mdl = 'pendulum';
 load_system(mdl);
-data = load_pendulum_data();
